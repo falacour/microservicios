9ff5cb2c53f0778ac6c03622d6a679d9c861d5fa
falacour <94493464+falacour@users.noreply.github.com>
2024-12-18 20:21:33 -0300

    se agrega configuracion a archivo aplication.properties

---
+++ autenticacion-server/src/test/java/com/falacour/autenticacion_server/AutenticacionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falacour.autenticacion_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
